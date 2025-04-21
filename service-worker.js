2f567d21c41920a35ed73a27add51874b710ad7e
Raimundo Emanuel <raimundoemanuel94@gmail.com>
2025-04-21 17:47:49 -0400

    Corrige manifest e service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,20 @@
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('tessy-cache').then(function (cache) {
+    caches.open('tessy-cache').then(function(cache) {
       return cache.addAll([
         './',
         './index.html',
         './manifest.json',
         './icon-192.png',
-        './icon-512.png',
-        './landing.html'
+        './icon-512.png'
       ]);
     })
   );
-  console.log('📦 Service Worker instalado e cache feito!');
 });
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function (response) {
+    caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
   );
